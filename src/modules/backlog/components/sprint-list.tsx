8e82212d672186766db750c7f1f31d5eaff3cc30
huynam-dana <nam.trinh@danaexperts.io.vn>
2025-08-04 18:11:14 +0700

    chore: remove comment

--- src/modules/backlog/components/sprint-list.tsx
+++ src/modules/backlog/components/sprint-list.tsx
@@ -4,7 +4,6 @@ import { SprintItem } from "./sprint-item";
 export const SprintList = () => {
   return (
     <div>
-      {/* Sprint items would go here */}
       <div className="flex flex-col gap-6">
         <SprintItem />
       <SprintItem />
--- src/shared/api-client.ts
+++ src/shared/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://api.example.com/v1", // 👈 Base URL của bạn
+  baseURL: "https://api.example.com/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
