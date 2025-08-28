a80e84863a8b3735a00ee62f6516db5235cf9a98
huynam-dana <nam.trinh@danaexperts.io.vn>
2025-08-28 19:52:47 +0700

    chore: refactor lint

--- src/shared/apiClient.ts
+++ src/shared/apiClient.ts
@@ -9,6 +9,31 @@ export const axiosClient = axios.create({
   },
 }); 
 
+
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+let refreshment = false;
+axiosClient.interceptors.response.use((response) => {
+  return response;
+}, async (error) => {
+  if (error.response.status === 401 ) {
+    if (!refreshment) {
+      refreshment = true;
+      const { data } = await apiClient.post("/auth/refresh", {
+        refreshToken: localStorage.getItem("refreshToken"),
+      });
+      localStorage.setItem("token", (data as { accessToken: string }).accessToken);
+      refreshment = false;
+    }
+  }
+});
+
 export type ApiClientConfig = {
   url?: string;
   headers?: RawAxiosRequestHeaders | AxiosHeaders;
